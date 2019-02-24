0010cd00d13ccce38307e10a4cab9f5068d0ed15
Ondřej Světlík <ondrej.svetlik@unicorn.com>
2019-02-24 22:20:41 +0100

    HTTP Auth, first request sent

--- extension.js
+++ extension.js
@@ -1,53 +1,91 @@
 
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
+const Lang = imports.lang;
+const PanelMenu = imports.ui.panelMenu;
+const Soup = imports.gi.Soup;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Lib = Me.imports.convenience;
 
-let text, button;
+const SERVICE_QUERY_PARAMS = {
+	"query": "servicelist",
+	"servicestatus": "warning critical unknown"
+};
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
+const NagiosChecker = new Lang.Class({
+	Name: 'NagiosChecker',
+	Extends: St.Bin,
 
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
+	_init: function () {
+		this.parent({
+			style_class: 'panel-button',
+			reactive: true,
+			can_focus: true,
+			x_fill: true,
+			y_fill: false,
+			track_hover: true
+		});
+		this._currentStatus = null;
+		this._icon = new St.Icon({ icon_name: 'media-record-symbolic', style_class: 'system-status-icon' });
+		this.set_child(this._icon);
+		// this.connect('button-press-event', doSomething());
+		// this._currentStatus = 'warn';
+		// this.connect('button-press-event', Lang.bind(this, this._setStatus));
 
-    text.opacity = 255;
+		this._settings = Lib.getSettings();
+		this._http = new Soup.Session({
+			"user-agent": "nagios-checker/" + Me.metadata['version']
+		});
+		this._http.connect('authenticate', Lang.bind(this, this._authenticate));
 
-    let monitor = Main.layoutManager.primaryMonitor;
+		this._refresh();
+	},
 
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
+	_authenticate: function(session, msg, auth, retrying) {
+		if (!retrying) {
+			auth.authenticate(this._settings.get_string('username'), this._settings.get_string('password'));
+		}
+	},
 
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
-}
+	_setStatus: function () {
+		this._icon.style_class = 'system-status-icon';
+		if (this._currentStatus) {
+			this._icon.add_style_class_name('nagios-status-' + this._currentStatus);
+		}
+	},
 
-function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
+	_refresh: function() {
+		this._icon.set_icon_name('document-open-recent-symbolic');
+		let msg = Soup.form_request_new_from_hash('GET', this._settings.get_string('url') + 'statusjson.cgi', SERVICE_QUERY_PARAMS);
+		this._http.queue_message(msg, Lang.bind(this, this._processReply));
+	},
 
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
-}
+	_processReply: function(session, message) {
+		this._icon.set_icon_name('media-record-symbolic');
+		if (message.status_code !== 200) {
+			this._currentStatus = "failed";
+		}
+		else {
+			let json = JSON.parse(message.response_body.data);
+			this._currentStatus = "ok";
+		}
+
+		this._setStatus();
+	}
+});
+
+let nagiosChecker;
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+	nagiosChecker = new NagiosChecker();
+    Main.panel._rightBox.insert_child_at_index(nagiosChecker, 0);
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+    Main.panel._rightBox.remove_child(nagiosChecker);
+    nagiosChecker.destroy();
+    nagiosChecker = null;
 }
+
+
